161374bfb93f743f7a78be606311cd5b70e53207
Daniel Alejandro Lopez Munera <danielalejandrolopez97@gmail.com>
2020-08-10 21:34:11 +0200

    Decimal Comparator.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Are the first 3 decimal places equals? "+ DecimalComparator.areEqualByThreeDecimalPlaces(1.723,1.724));
+
+    }
+}
